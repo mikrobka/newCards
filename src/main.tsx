b561ca9504700704acbdf909fa1c52c0f1be97ef
Andrey Paramonov <mikrobka231@gmail.com>
2023-07-04 23:09:29 +0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+import { StrictMode } from 'react'
+import * as React from 'react'
+
+import { createRoot } from 'react-dom/client'
+
+import { App } from './App'
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
