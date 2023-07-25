7f163b1e96b99c5bb723f3966372928e3653ba4a
Andrey Paramonov <mikrobka231@gmail.com>
2023-07-26 00:30:56 +0300

    refactor: change lock file package.json eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,22 @@
 module.exports = {
-    extends: ['@it-incubator/eslint-config', 'plugin:storybook/recommended', "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:import/errors",
-        "plugin:import/warnings",
-        "plugin:import/typescript"],
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint", "import"],
-    rules: {
-        'no-console': ['warn', {
-            allow: ['warn', 'error']
-        }]
-    },
-    "settings": {
-        "import/resolver": {
-            "typescript": {} // This enables eslint-import-resolver-typescript
-        }
-    },
+    extends: ['@it-incubator/eslint-config'],
+    // parser: "@typescript-eslint/parser",
+    // plugins: ["@typescript-eslint", "import"],
+    // rules: {
+    //     'no-console': ['warn', {
+    //         allow: ['warn', 'error']
+    //     }]
+    // },
+    // "settings": {
+    //     "import/resolver": {
+    //         "typescript": {} // This enables eslint-import-resolver-typescript
+    //     }
+    // },
+
+    // , 'plugin:storybook/recommended', "eslint:recommended",
+    // "plugin:@typescript-eslint/recommended",
+    // "plugin:import/errors",
+    // "plugin:import/warnings",
+    // "plugin:import/typescript"
 
 };
