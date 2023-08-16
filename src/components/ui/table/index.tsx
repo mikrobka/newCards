8006197aa162c108373ab693dafd53485e1d9182
Andrey Paramonov <mikrobka231@gmail.com>
2023-08-16 19:04:59 +0300

    fetch: add table

---
+++ src/components/ui/table/index.tsx
@@ -0,0 +1,67 @@
+import { logDOM } from '@storybook/testing-library'
+import * as console from 'console'
+import React, { ComponentPropsWithoutRef, MouseEventHandler } from 'react'
+
+export type Sort = {
+  key: string
+  direction: 'asc' | 'desc'
+} | null
+
+export type Colum = {
+  title: string
+  key: string
+  isSortable?: boolean
+}
+
+type Props = ComponentPropsWithoutRef<'thead'> & {
+  columns: Array<Colum>
+  sort: Sort
+  onSort: (sort: Sort) => void
+}
+
+const dataAttributes = {
+  sortable: 'data-sortable',
+  name: 'data-key',
+} as const
+
+export const TableHeader = ({ columns, sort, onSort }: Props) => {
+  const handleSorting: MouseEventHandler<HTMLTableHeaderCellElement> = e => {
+    const isSortable = e.currentTarget.getAttribute(dataAttributes.sortable)
+    const key = e.currentTarget.getAttribute(dataAttributes.name)
+
+    if (key === null) throw new Error('No data-name')
+
+    if (!isSortable) return
+    if (key !== sort?.key) {
+      return onSort({ key, direction: 'asc' })
+    }
+    if (sort.direction === 'asc') {
+      return onSort({ key, direction: 'desc' })
+    }
+
+    onSort(null)
+  }
+
+  return (
+    <thead>
+      <tr>
+        {columns.map(colum => {
+          const showSort = colum.isSortable && sort && sort.key === colum.key
+
+          return (
+            <th
+              {...{
+                [dataAttributes.sortable]: colum.isSortable,
+                [dataAttributes.name]: colum.key,
+              }}
+              key={colum.title}
+              onClick={handleSorting}
+            >
+              {colum.title} {showSort && <span>{sort?.direction === 'asc' ? '▲' : '▼'}</span>}
+            </th>
+          )
+        })}
+      </tr>
+    </thead>
+  )
+}
---
+++ src/components/ui/table/table.stories.tsx
@@ -0,0 +1,102 @@
+import React, { ComponentProps, useState } from 'react'
+
+import { Colum, Sort, TableHeader } from './'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<ComponentProps<typeof TableHeader>> = {
+  title: 'Components/Table',
+  component: TableHeader,
+  tags: ['autodocs'],
+}
+const data = [
+  {
+    title: 'Project A',
+    cardsCount: 10,
+    updated: '2023-07-07',
+    createdBy: 'John Doe',
+  },
+  {
+    title: 'Project B',
+    cardsCount: 5,
+    updated: '2023-07-06',
+    createdBy: 'Jane Smith',
+  },
+  {
+    title: 'Project C',
+    cardsCount: 8,
+    updated: '2023-07-05',
+    createdBy: 'Alice Johnson',
+  },
+  {
+    title: 'Project D',
+    cardsCount: 3,
+    updated: '2023-07-07',
+    createdBy: 'Bob Anderson',
+  },
+  {
+    title: 'Project E',
+    cardsCount: 12,
+    updated: '2023-07-04',
+    createdBy: 'Emma Davis',
+  },
+]
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  // @ts-ignore
+  args: {},
+}
+
+export const WithSort = {
+  render: () => {
+    const [sort, setSort] = useState<Sort>(null)
+
+    const columns: Colum[] = [
+      {
+        key: 'name',
+        title: 'Name',
+        isSortable: true,
+      },
+      {
+        key: 'cardsCount',
+        title: 'Cards Count',
+        isSortable: true,
+      },
+      {
+        key: 'updated',
+        title: 'Updated',
+        isSortable: true,
+      },
+      {
+        key: 'createdBy',
+        title: 'Created By',
+        isSortable: true,
+      },
+      {
+        key: 'options',
+        title: '',
+        isSortable: false,
+      },
+    ]
+
+    return (
+      <table>
+        <TableHeader columns={columns} sort={sort} onSort={setSort} />
+        <tbody>
+          {data.map(item => (
+            <tr key={item.title}>
+              <td>{item.title} </td>
+              <td> {item.cardsCount} </td>
+              <td> {item.updated} </td>
+              <td> {item.createdBy} </td>
+              <td> icons...</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )
+  },
+}
