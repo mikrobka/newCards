2d5354854bc82898ef0f88cea1c34a5518fb4a6f
Andrey Paramonov <mikrobka231@gmail.com>
2023-09-26 14:38:03 +0300

    fetch: add slider component

--- src/components/ui/slider/slider.stories.ts
+++ src/components/ui/slider/slider.stories.ts
@@ -1,18 +1,64 @@
-import { ComponentProps } from 'react'
+import { useState } from 'react'
 
-import { Slider } from './'
+import { Meta, StoryObj } from '@storybook/react'
+import { Slider } from './index'
 
-import type { Meta, StoryObj } from '@storybook/react'
-
-const meta: Meta<ComponentProps<typeof Slider>> = {
+const meta: Meta<typeof Slider> = {
   title: 'Components/Slider',
   component: Slider,
   tags: ['autodocs'],
-}
+  argTypes: {
+    multiple: {
+      control: { type: 'boolean' },
+    },
+    disabled: {
+      control: { type: 'boolean' },
+    },
+  },
+} satisfies Meta<typeof Slider>
 
 export default meta
-type Story = StoryObj<typeof meta>
+type Story = StoryObj<typeof Slider>
 
 export const Default: Story = {
-  args: {},
+  render: args => {
+    const [value, setValue] = useState(30)
+
+    const changeHandler = (value: number[]) => setValue(value[0])
+
+    return <Slider {...args} value={[value]} multiple onChange={changeHandler} />
+  },
 }
+export const Disabled: Story = {
+  render: args => {
+    const [value, setValue] = useState(30)
+
+    const changeHandler = (value: number[]) => setValue(value[0])
+
+    return <Slider {...args} value={[value]} disabled={true} multiple onChange={changeHandler} />
+  },
+}
+
+export const Multiple: Story = {
+  render: args => {
+    const [value, setValue] = useState([30, 60])
+
+    const changeHandler = (value: number[]) => setValue(value)
+
+    return <Slider {...args} value={value} multiple onChange={changeHandler} />
+  },
+}
+
+export const WithLabel: Story = {
+  render: args => {
+    const [value, setValue] = useState([30, 60])
+
+    const changeHandler = (value: number[]) => setValue(value)
+
+    return (
+      <Slider {...args} value={value} label={'Number of cards'} multiple onChange={changeHandler} />
+    )
+  },
+}
+
+export const WithoutPropsValue: Story = {}
