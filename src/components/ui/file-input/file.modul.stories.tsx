7f3c6caba8dbe3a9c8504bfd7e06809fd3128cc7
Andrey Paramonov <mikrobka231@gmail.com>
2023-09-29 21:34:49 +0300

    fetch: add file input component

---
+++ src/components/ui/file-input/file.modul.stories.tsx
@@ -0,0 +1,54 @@
+import { Meta, StoryObj } from '@storybook/react'
+import React, { useState } from 'react'
+
+import { Button } from '@components/ui'
+import { FileInput } from '@components/ui/file-input/index'
+
+const meta = {
+  title: 'Components/FileInput',
+  component: FileInput,
+  tags: ['autodocs'],
+  argTypes: {
+    disabled: {
+      control: { type: 'boolean' },
+    },
+  },
+} satisfies Meta<typeof FileInput>
+
+export default meta
+type Story = StoryObj<typeof FileInput>
+
+export const UploadWithButton: Story = {
+  render: args => {
+    const [file, setFile] = useState('')
+    const changeFileHandler = (file: string) => setFile(file)
+
+    return (
+      <div>
+        <FileInput
+          {...args}
+          onChange={changeFileHandler}
+          trigger={
+            <Button variant={'secondary'} disabled={args.disabled}>
+              Upload file
+            </Button>
+          }
+        />
+        <div>{file}</div>
+      </div>
+    )
+  },
+}
+export const UploadWithIcon: Story = {
+  render: args => {
+    const [file, setFile] = useState('')
+    const changeFileHandler = (file: string) => setFile(file)
+
+    return (
+      <div>
+        <FileInput {...args} onChange={changeFileHandler} trigger={<EditIcon />} />
+        <div>{file}</div>
+      </div>
+    )
+  },
+}
---
+++ src/components/ui/file-input/index.tsx
@@ -0,0 +1,55 @@
+import clsx from 'clsx'
+import { ChangeEvent, ReactNode, useRef } from 'react'
+
+import s from './file-input.module.scss'
+
+const convertFileToBase64 = (file: File, callback: (value: string) => void) => {
+  const reader = new FileReader()
+
+  reader.onloadend = () => {
+    const file64 = reader.result as string
+
+    callback(file64)
+  }
+  reader.readAsDataURL(file)
+}
+
+type FileInputPropsType = {
+  onChange: (file: string) => void
+  disabled?: boolean
+  trigger: ReactNode
+}
+
+export const FileInput = (props: FileInputPropsType) => {
+  const { onChange, disabled = false, trigger } = props
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+  const handleUploadClick = () => inputRef.current?.click()
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length) {
+      const file = e.target.files[0]
+
+      if (file.size < 1000000) {
+        convertFileToBase64(file, file64 => {
+          onChange(file64)
+        })
+      }
+    }
+  }
+
+  const classNames = {
+    container: s.container,
+    trigger: clsx(s.trigger, disabled && s.disabled),
+    input: s.input,
+  }
+
+  return (
+    <div className={classNames.container}>
+      <span onClick={handleUploadClick} className={classNames.trigger}>
+        {trigger}
+      </span>
+      <input type="file" ref={inputRef} onChange={handleFileChange} className={classNames.input} />
+    </div>
+  )
+}
